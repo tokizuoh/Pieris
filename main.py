080fb9fffa8f7103bc9d360254ed4fc35a2482ec
tokizuoh <k_sakai@yumemi.co.jp>
2021-04-04 21:32:52 +0900

    add main.py (指定したディレクトリ以下の特定の拡張子をもつファイルを抽出する処理を追加)

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import pathlib
+
+TARGET_EXPAND = '.swift'
+
+def main():
+    # TODO: 動的に
+    path = '/Users/tokizo/prg/sw/Pendula/Pendula'
+
+    p = pathlib.Path(path)
+    for f in p.glob('**/*'):
+        file_path = str(f)
+
+        if not os.path.isfile(file_path) or not isSameExpand(expand=TARGET_EXPAND, file_path=file_path):
+            continue
+        print(file_path)
+        
+        # TODO: ファイル内検索
+        # with open(file_path, 'r') as file:
+            # print(file)
+            # exit()
+
+def isSameExpand(expand: str, file_path: str) -> bool:
+    return expand == file_path[-len(expand):]
+
+main()
