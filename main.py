f7b35c959a875d7fe09abeaa462e9ed75b363f1e
tokizuoh <k_sakai@yumemi.co.jp>
2021-04-05 19:36:13 +0900

    継承されていないクラス名、継承されているクラス名の抽出処理を追加

--- main.py
+++ main.py
@@ -8,20 +8,32 @@ def main():
     path = '/Users/tokizo/prg/sw/Pendula/Pendula'
 
     p = pathlib.Path(path)
-    for f in p.glob('**/*'):
+    
+    inherited_class_names = set()
+    class_names = []
+
+    file_paths = p.glob('**/*')
+    for f in file_paths:
         file_path = str(f)
 
         if not os.path.isfile(file_path) or not isSameExpand(expand=TARGET_EXPAND, file_path=file_path):
             continue
-        print(file_path)
         
-        # TODO: ファイル内検索
-        # with open(file_path, 'r') as file:
-            # print(file)
-            # exit()
+        for line in  open(file_path, 'r'):
+            words = line.split()
+
+            if len(words) == 0 or words[0] != 'class':
+                continue
+
+            class_names.append(words[1][:-1])
+            [inherited_class_names.add(i) for i in extract_inherited_class_names(words)]
+
 
 # TODO: 命名がおかしい sameであるなら比較対象はどちらも同じ型であるべき？
 def isSameExpand(expand: str, file_path: str) -> bool:
     return expand == file_path[-len(expand):]
+
+def extract_inherited_class_names(line: str) -> [str]:
+    return line[2:-1]
 
 main()
