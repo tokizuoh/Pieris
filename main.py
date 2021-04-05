58a041f6e5ae13f4888ae527123a27878801ae39
tokizuoh <k_sakai@yumemi.co.jp>
2021-04-05 19:55:22 +0900

    classの情報としてファイル名、ファイルパスをもたせるように修正

--- main.py
+++ main.py
@@ -3,20 +3,30 @@ import pathlib
 
 TARGET_EXPAND = '.swift'
 
+
+class ClassFile:
+
+    name: str
+    path: str
+
+    def __init__(self, name, path):
+        self.name = name
+        self.path = path
+
+
 def main():
     # TODO: 動的に
     path = '/Users/tokizo/prg/sw/Pendula/Pendula'
 
     p = pathlib.Path(path)
-    
     inherited_class_names = set()
-    class_names = []
+    class_files: [ClassFile] = []
 
     file_paths = p.glob('**/*')
     for f in file_paths:
         file_path = str(f)
 
-        if not os.path.isfile(file_path) or not is_same_expand(expand=TARGET_EXPAND, file_path=file_path):
+        if not os.path.isfile(file_path) or not has_expand(expand=TARGET_EXPAND, file_path=file_path):
             continue
         
         for line in  open(file_path, 'r'):
@@ -25,16 +35,18 @@ def main():
             if len(words) == 0 or words[0] != 'class':
                 continue
 
-            class_names.append(words[1][:-1])
+            class_files.append(ClassFile(name=words[1][:-1], path=file_path))
             [inherited_class_names.add(i) for i in extract_inherited_class_names(words)]
 
-    not_inherited_class_names = [class_name for class_name in class_names if class_name not in inherited_class_names]
+    not_inherited_class_files = [class_name for class_name in class_files if class_name not in inherited_class_names]
 
-# TODO: 命名がおかしい sameであるなら比較対象はどちらも同じ型であるべき？
-def is_same_expand(expand: str, file_path: str) -> bool:
+
+def has_expand(expand: str, file_path: str) -> bool:
     return expand == file_path[-len(expand):]
+
 
 def extract_inherited_class_names(line: str) -> [str]:
     return line[2:-1]
+
 
 main()
